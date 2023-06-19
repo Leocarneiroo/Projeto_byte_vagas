b75381a917949a17503731493b8ce45c51556017
Leonardo Carneiro <leocarneiroo@gmail.com>
2023-06-19 18:20:53 -0300

    up to date home web and mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,63 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import bgBlur from './src/assets/bg-blur.png'
+import Coracao from './src/assets/coracao.svg'
 
 // medidas no ambiente mobile são diferentes de pixels por isso nao se usa px nas medidas
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="bg-zinc-950 flex-1 items-center justify-center">
-      <Text className="text-zinc-50 font-bold text-4xl">
-        Projeto Byte Vagas
+    <ImageBackground
+      source={bgBlur}
+      className=" realtive flex-1 items-center bg-zinc-900 px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <View className="flex-1 items-center justify-center gap-6">
+        <Coracao />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Transforme o mundo com a sua jornada!
+          </Text>
+          <Text className="p-5 text-center font-body text-base leading-relaxed text-gray-50">
+            Descubra seu potencial: Encontre a melhor empresa para otimizar seus
+            talentos!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-400 px-5 py-3"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            CADASTRAR CURRICULUM
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com carinho por Equipe Byte
       </Text>
+
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,63 @@
-import { Button } from '../components/Button'
+import {User} from 'lucide-react'
+import Image from 'next/image'
+
+import coracao from '../assets/coracao.svg'
+
 export default function Home() {
-  return (
-    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
-      <h1 className="text-4x1 font-bold">Byte Project</h1>
-      <Button title="Hello" />
+  return <main className="grid grid-cols-2 min-h-screen">
+    {/*Parte esquerda*/}
+    <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+      {/*Blur da cor roxa*/}
+      <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-500 opacity-50 -translate-y-1/2 translate-x-1/2 rounded-full blur-full" />
+      
+      {/*Sing in button*/}
+      <a href="" className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
+        
+        {/*Ícone de usuário*/}
+        <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-100">
+          <User className='h-5 w-5 text-gray-500' />
+        </div>
+
+        <p className='max-w-[140px] text-sm leading-snug'><span className='underline'>Crie sua conta</span></p>
+      </a>
+
+      {/*Imagem do coração*/}
+      <div className='space-y-5'>
+        <Image src={coracao} alt="Imagem coração" />
+
+        {/*Título*/}
+        <div className='max-w-[420px] space-y-4'>
+          <h1 className='mt-5 text-4xl font-bold leading-tight text-gray-50'>
+            Transforme o mundo com a sua jornada!
+          </h1>
+          {/*Subtítulo*/}
+          <p className='mt-1 text-lg leading-relaxed'>
+            Descubra seu potencial: Encontre a melhor empresa para otimizar seus talentos!
+          </p>
+        </div>
+
+        {/*Botão de cadastro - abaixo do título*/}
+        <a className='inline-block rounded-full bg-green-400 px-5 py-3 font-alt text-sm leading-none uppercase text-black hover:bg-green-600' href="">
+          CADASTRAR CURRICULUM
+        </a>
+      </div>
+
+      {/*Copyright*/}
+      <div className='text-sm leading-relaxed text-gray-200'>
+        Feito com carinho por Equipe Byte.
+      </div>
+
     </div>
-  )
+
+
+    {/*Parte direita*/}
+    <div className="flex lfex-col p-16">
+      <div className="flex flex-1 items-center justify-center">
+        <p className="text-center leading-relaxed w-[360px]">
+          Você ainda não cadastrou o seu CV,{' '}
+          <a href="" className="underline text-gray-50">cadastre aqui</a>
+        </p>
+      </div>
+    </div>
+  </main>
 }
